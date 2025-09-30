8114712656cdb3fd5ab4fcbc275c5a6ac9742dd6
yasser100ali <108766553+yasser100ali@users.noreply.github.com>
2025-09-29 19:33:05 -0700

    added refresh button

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,6 +5,10 @@ import { GitIcon, VercelIcon } from "./icons";
 import Link from "next/link";
 
 export const Navbar = () => {
+  const handleRefresh = () => {
+    window.location.reload();
+  };
+
   return (
     <div className="p-2 flex flex-row gap-2 justify-between">
       <Link href="https://github.com/yasser100ali/law-ai">
@@ -13,6 +17,23 @@ export const Navbar = () => {
         </Button>
       </Link>
 
+      <Button variant="outline" onClick={handleRefresh}>
+        <svg 
+          xmlns="http://www.w3.org/2000/svg" 
+          width="16" 
+          height="16" 
+          viewBox="0 0 24 24" 
+          fill="none" 
+          stroke="currentColor" 
+          strokeWidth="2" 
+          strokeLinecap="round" 
+          strokeLinejoin="round"
+          className="mr-2"
+        >
+          <path d="M21.5 2v6h-6M2.5 22v-6h6M2 11.5a10 10 0 0 1 18.8-4.3M22 12.5a10 10 0 0 1-18.8 4.2"/>
+        </svg>
+        Refresh
+      </Button>
     </div>
   );
 };
