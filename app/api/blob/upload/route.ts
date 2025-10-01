39ec77e5a0107869fa58bccbb0ddd339712c1ab3
yasser100ali <108766553+yasser100ali@users.noreply.github.com>
2025-10-01 10:06:13 -0700

    implemented route.ts, testing to vercel now

--- app/api/blob/upload/route.ts
+++ app/api/blob/upload/route.ts
@@ -1,10 +1,34 @@
-import { NextReponse } from "next/server";
+import { NextResponse } from "next/server";
 import { handleUpload, type HandleUploadBody} from '@vercel/blob/client';
 
 export const runtime = "edge"
 
-export async function POST(request: Request): Promise<NextReponse> {
-    const body = (await request.join()) as HandleUploadBody;
+export async function POST(request: Request): Promise<NextResponse> {
+    const body = (await request.json()) as HandleUploadBody;
 
-    try { }
+    try {
+        const json = await handleUpload({
+            body, 
+            request,
+            onBeforeGenerateToken: async(pathname) => {
+                return {
+                    allowedContentTypes: [
+                        "application/pdf", 
+                        "text/plain",
+                        "text/csv",
+                        "application/vnd.ms-excel",
+                        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                    ],
+                    addRandomSuffix: true,
+                };
+            },
+            onUploadCompleted: async ({blob}) => {
+                console.log("blob uploaded successfully", blob.url, blob.pathname, blob.contentType);
+            },
+        });
+        
+        return NextResponse.json(json);
+    } catch(err) { 
+        return NextResponse.json({ error: (err as Error).message }, { status: 400 });
+    }
 }
