97649222f6b941dc59a6d8581a0d90642e24476d
yasser100ali <108766553+yasser100ali@users.noreply.github.com>
2025-10-03 14:12:03 -0700

    delete button works locally now, testing to see if it works on vercel deployment

--- next.config.js
+++ next.config.js
@@ -3,11 +3,11 @@ const nextConfig = {
   rewrites: async () => {
     return [
       {
-        source: "/api/:path*",
+        source: "/api/chat/:path*",
         destination:
           process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/api/:path*"
-            : "/api/",
+            ? "http://127.0.0.1:8000/api/chat/:path*"
+            : "/api/chat/:path*",
       },
       {
         source: "/docs",
